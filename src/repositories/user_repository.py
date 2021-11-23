1a993e03e5f0ed6f280b7f46b7a5569fbfa6b87f
Henrik Hautajärvi <henrik.hautajarvi@gmail.com>
2021-11-23 18:15:00 +0200

    fixed invoke commands and database initialization

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -5,14 +5,16 @@ class UserRepository:
     def __init__(self, connection):
         self.connection = connection
 
-    def create_user(self, user):
+    def create_user(self, username, password):
         cursor = self.connection.cursor()
-        cursor.execute("INSERT INTO Users (username, password) VALUES (?, ?)", (user.username, user.password))
+        cursor.execute("INSERT INTO Users (username, password) VALUES (?, ?)", (username, password))
         self.connection.commit()
+        user = User(username, password)
         return user
 
     def find_user(self, username, password):
         cursor = self.connection.cursor()
         cursor.execute("SELECT username, password FROM Users WHERE username=? AND password=?", (username, password))
         name = cursor.fetchone()
-        return name
+        user = User(name[0], name[1])
+        return user
--- src/ui/login.py
+++ src/ui/login.py
@@ -1,51 +1,60 @@
 from tkinter import ttk, constants
+from repositories.user_repository import UserRepository
 
 class LoginView():
-    def __init__(self, root):
+    def __init__(self, root, budget_create, budget_view):
         self.root = root
         self.username = None
         self.password = None
         self.newuser = None
         self.newpass = None
+        self.frame = None
+        self.budget_create = budget_create
+        self.budget_view = budget_view
 
         self.initialize()
+
+    def pack(self):
+        self.frame.pack(fill=constants.X)
 
     def show_login(self):
         login_label = ttk.Label(master=self.frame, text="Login")
         username_label = ttk.Label(master=self.frame, text="Username")
-        username_entry = ttk.Entry(master=self.frame)
+        self.username_entry = ttk.Entry(master=self.frame)
 
         password_label = ttk.Label(master=self.frame, text="Password")
-        password_entry = ttk.Entry(master=self.frame)
+        self.password_entry = ttk.Entry(master=self.frame)
 
-        loginbutton = ttk.Button(master=self.frame, text="Login", command=self.loginbutton_click(username_entry.get(), password_entry.get()))
         login_label.grid(row=1, column=0, columnspan=2, sticky=constants.W, padx=5, pady=5)
 
         username_label.grid(padx=5, pady=5)
-        username_entry.grid(row=2, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
+        self.username_entry.grid(row=2, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
 
         password_label.grid(padx=5, pady=5)
-        password_entry.grid(row=3, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
+        self.password_entry.grid(row=3, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
+
+        loginbutton = ttk.Button(master=self.frame, text="Login", command=self.loginbutton_click)
         loginbutton.grid(row=4, column=0, columnspan=2, sticky=(constants.E, constants.W), padx=5, pady=5)
+
 
     def show_register(self):
         register_label = ttk.Label(master=self.frame, text="Make a new user")
 
         newuser_label = ttk.Label(master=self.frame, text= "Choose a username")
-        newuser_entry = ttk.Entry(master=self.frame)
+        self.newuser_entry = ttk.Entry(master=self.frame)
 
         newpassword_label = ttk.Label(master=self.frame, text="Choose a password")
-        newpassword_entry = ttk.Entry(master=self.frame)
-        registerbutton = ttk.Button(master=self.frame, text="Register", command=self.registerbutton_click(newuser_entry.get(), newpassword_entry.get()))
+        self.newpassword_entry = ttk.Entry(master=self.frame)
         
         register_label.grid(row=5, column=0, columnspan=2, sticky=constants.W, padx=5, pady=5)
 
         newuser_label.grid(padx=5, pady=5)
-        newuser_entry.grid(row=6, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
+        self.newuser_entry.grid(row=6, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
 
         newpassword_label.grid(padx=5, pady=5)
-        newpassword_entry.grid(row=7, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
+        self.newpassword_entry.grid(row=7, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
 
+        registerbutton = ttk.Button(master=self.frame, text="Register", command=self.registerbutton_click)
         registerbutton.grid(row=8, column=0, columnspan=2, sticky=(constants.E, constants.W), padx=5, pady=5)
 
     def initialize(self):
@@ -57,10 +66,12 @@ class LoginView():
         self.show_login()
         self.show_register()
 
-        self.root.grid_columnconfigure(1, weight=1, minsize=300)
+        self.frame.grid_columnconfigure(1, weight=1, minsize=300)
 
-    def loginbutton_click(self, username_entry, password_entry):
-        pass
+    def loginbutton_click(self):
+        user = UserRepository.find_user(self.username_entry.get(), self.password_entry.get())
+        self.budget_view(user)
 
-    def registerbutton_click(self, newuser_entry, newpassword_entry):
-        pass
+    def registerbutton_click(self):
+        user = UserRepository.create_user(self.newuser_entry.get(), self.newpassword_entry.get())
+        self.budget_create(user)
